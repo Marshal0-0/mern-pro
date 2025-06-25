b5e44ae7333666c64c0b0770362e1b3cd9b7d9bf
Marshal0-0 <minageorgerezkatallah@gmail.com>
2025-06-25 09:36:53 +0300

    Refactor docker-compose.yml to remove .env file references and use MONGO_URI for MongoDB connections in Cart, Product, and User services

--- User/config/db_conn.js
+++ User/config/db_conn.js
@@ -12,12 +12,11 @@ const mongo_username = process.env.MONGO_USERNAME;
 const mongo_password = process.env.MONGO_PASSWORD;
 const mongo_cluster = process.env.MONGO_CLUSTER;
 const mongo_database = process.env.MONGO_DBNAME;
+const mongo_uri = process.env.MONGO_URI;
 
-
-mongoose.connect(`mongodb+srv://${mongo_username}:${mongo_password}@${mongo_cluster}/${mongo_database}?retryWrites=true&w=majority`
-, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log(`Connected to: ${mongoose.connection.name}`))
-.catch(err => console.log(err));
+mongoose.connect(mongo_uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(`Connected to: ${mongoose.connection.name}`))
+  .catch(err => console.log(err));
 
 
 
